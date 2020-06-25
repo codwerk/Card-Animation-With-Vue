5bbb2125b199ebf4a844aecd671ffe28f6889d33
codwerk <louis_widdy@live.fr>
2020-06-25 18:01:09 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
-<template>
+<template lang="pug">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Card
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
 <style>
+
+@import url('./assets/scss/index.scss');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+ 
+</template>
+
+<script>
+ export default {
+   name:'Card',
+   data: () => {
+     return {}
+   }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
